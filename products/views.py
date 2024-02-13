24eeabb8d0b3e4a83499f4dacbdab6f174fbee4c
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-13 13:37:54 +0000

    Created products folder and added products.html. Created template to render products created in admin panel

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def all_products(request):
+    """
+    View to return products.html
+    """
+    products = Product.objects.all()
+    #Context sends data stored in product key to template products.html
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
