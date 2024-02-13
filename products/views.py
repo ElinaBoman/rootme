63c49e012e68a9f8291f4dceb3847e7bea66fc0b
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-13 16:49:05 +0000

    Added product_detail.html and defined it in product views

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
@@ -11,3 +11,15 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    View to return products_detail.html
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    #Context sends data stored in product key to template products.html
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
