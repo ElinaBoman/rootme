4d7c255ffcda84ea248deb9adeea3e26902634fd
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-14 07:48:47 +0000

    Added Q to imports and started creating search queries in products view.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+# Imports search query, q will fetch product that has search in either name or description.
+from django.db.models import Q
 from .models import Product
 
 def all_products(request):
@@ -6,10 +9,20 @@ def all_products(request):
     View to return products.html
     """
     products = Product.objects.all()
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter search criteria!")
+                return redirect(reverse('products'))
+            queries = Q(name_icontain=query) | Q(description__icontain=query)
+            products = products.filter(queries)
     #Context sends data stored in product key to template products.html
     context = {
         'products': products,
-    }
+        'search_term': query
+,    }
     return render(request, 'products/products.html', context)
 
 
