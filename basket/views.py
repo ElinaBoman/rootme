ecf20245c8f0519d01c1cee70a882bfcc1e5a564
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-15 09:24:39 +0000

    Added basket urls to project and created basket urls.py

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_basket(request):
+    """
+    View to return basket.html
+    """
+
+    return render(request, 'basket/basket.html')
