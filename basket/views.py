ddfcd512db5d101084cdba055156e2a48120dcda
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-16 14:36:51 +0000

    Fixed adjust_bag functions to be able to update and remove products from basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_basket(request):
@@ -10,11 +10,8 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
     """Add products to shopping basket"""
-   
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    print('redirect url'+ redirect_url)
-    print(request.path)
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
@@ -25,6 +22,25 @@ def add_to_basket(request, item_id):
     request.session['basket'] = basket
 
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+
+def remove_from_basket(request, item_id):
+    basket = request.session.get('basket', {})
+    basket.pop(item_id)
+    return HttpResponse(status=200)
 
 
 
