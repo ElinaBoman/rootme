51b2205352af71483b9c78afd61f02fb50efd3d7
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-19 14:13:16 +0000

    Created Order and OrderLiteItems models inside checkout app

--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import uuid
+from django.conf import settings
+from django.db.models import Sum
 
-# Create your models here.
+from products.models import Product
+
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=32, null=False, editable=False)
+    user_name = models.CharField(max_length=60, null=False, blank=False)
+    email = models.EmailField(max_length=60, null=False, blank=False)
+    mobile_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=30, null=False, blank=False)
+    postalcode = models.CharField(max_length=30, null=False, blank=False)
+    city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, blank=True, null=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    data = models.DateTimeField(auto_now_add=True)
+    delivery_fee = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    total_sum = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
