70f61d9e48ac05a36a0361f2d8c24b1ee85e6f7c
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-19 09:54:47 +0000

    Added css for toasts in base.css

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
-
+from django.contrib import messages
+from products.models import Product
 
 
 
@@ -12,6 +13,8 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
     """Add products to shopping basket"""
+    print(Product)
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -20,7 +23,7 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
-    
+        messages.success(request, f'Added {product.name} to basket')
     request.session['basket'] = basket
 
     return redirect(redirect_url)
