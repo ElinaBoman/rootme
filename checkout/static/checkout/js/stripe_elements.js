509570ba95d10ca67c0994be9c4024ce1cf5aba4
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-21 15:27:29 +0000

    Added javascript for loading-overlay and payment-form

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -49,6 +49,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -73,3 +75,5 @@ form.addEventListener('submit', function(ev) {
         }
     });
 });
+
+
