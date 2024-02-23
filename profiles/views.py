5c1d64dd38d32a1d0e2fa6477f6568444ea730af
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-23 10:52:34 +0000

    Set up urls for new profiles app and created static folder and base template for profile.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile_view(request):
+    """
+    Display user profiles.
+    """
+
+    template = 'profiles/profile.html'
+    context = {}
+
+
+    return render(request, template, context)
