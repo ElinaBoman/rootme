6cc7a2fc67f9acdaac0b536bc170f0adec146bc3
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-23 15:09:01 +0000

    Updated profile views to show userprofileform, and started rendering order history

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile_view(request):
@@ -7,9 +8,18 @@ def profile_view(request):
     Display user profiles.
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+   
+    print('profile', profile)
+
+    orders = profile.orders.all()
+
+    print('order', orders)
+    
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders
     }
 
 
