8dbf4bf23b4a84014efdfd6b584fec35cfefd3cb
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-24 10:42:42 +0000

    Added view to update profileForm, and changed success toas to not show productinformation on profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -8,18 +9,19 @@ def profile_view(request):
     Display user profiles.
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid:
+            form.save()
+            messages.success(request, 'Profile has been updated successfully')
     form = UserProfileForm(instance=profile)
-   
-    print('profile', profile)
-
     orders = profile.orders.all()
 
-    print('order', orders)
-    
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        'orders': orders
+        'orders': orders,
+        'on_profile_view': True,
     }
 
 
