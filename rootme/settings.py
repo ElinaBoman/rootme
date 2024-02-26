4a59533eee397e221c4be1d972f84d5c3fc32de9
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-26 12:22:57 +0000

    Removed secret key and adjusted debug mode

--- rootme/settings.py
+++ rootme/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8d8umwuh0rjo5!xg)#lta83i=mte*02vu0(a8*8*eawh8(sowh'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['rootme-fcab110145ab.herokuapp.com', '8000-elinaboman-rootme-b8p8j873314.ws-eu108.gitpod.io']
 
