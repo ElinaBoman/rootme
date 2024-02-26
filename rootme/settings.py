ec1fa601eeb175b174f9cf813e15350e84f0037f
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-26 13:40:21 +0000

    cache controll in settings

--- rootme/settings.py
+++ rootme/settings.py
@@ -186,6 +186,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'rootme-fcab110145ab'
     AWS_S3_REGION_NAME = 'us-east-1'
@@ -202,7 +207,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
+
 DEFAULT_FROM_EMAIL = 'rootme@example.com'
 # Stripe
 STRIPE_CURRENCY = 'usd'
