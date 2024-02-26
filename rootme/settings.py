31dca95499605713a15f65c02aeeef4a75e1c025
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-26 16:19:53 +0000

    Added email settings in settings

--- rootme/settings.py
+++ rootme/settings.py
@@ -216,3 +216,16 @@ DEFAULT_FROM_EMAIL = 'rootme@example.com'
 STRIPE_CURRENCY = 'usd'
 FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_PERCENTAGE = 15
+
+# Email
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
