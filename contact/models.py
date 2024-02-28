04a1bdf97814ee72683bb7daf6664ce902684ddd
ElinaBoman <elinaholmberg@hotmail.com>
2024-02-28 13:31:24 +0000

    Added links to contact.html

--- contact/models.py
+++ contact/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 import datetime
 
-# Create your models here.
+
+
+
 
 class Contact(models.Model):
+
+    CATEGORY_CHOICES = {
+        ("Plants", "Plants"),
+        ("Shipping & Delivery", "Shipping & Delivery"),
+        ("Other", "Other")
+    }
+
     contact_email = models.EmailField(max_length=60, null=False, blank=False)
     contact_date = models.DateField(auto_now_add=True, null=True)
-    contact_subject = models.CharField(max_length=30, null=False, blank=False)
+    contact_subject = models.CharField(max_length=30, choices=CATEGORY_CHOICES,
+                                        null=False, blank=False)
     contact_message = models.TextField(max_length=500, null=False, blank=False)
--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .forms import ContactForm
-from checkout.models import Order
+from contact.models import Contact
+from .forms import ContactForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
 
 
 # Create your views here.
+def _send_confirmation_email():
+    """Send confirmation email to customer"""
+    customer_email = Contact.contact_email
+    subject = 'Q&A'
+    body = 'Thank you for reaching out, we will be in contact with you shortly!\nBest regards,\nRoot|Me'
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [customer_email]
+    )
+
 
 def contact_view(request):
     """
@@ -15,14 +32,19 @@ def contact_view(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your message has been sent, we will contact you shortly!')
+            _send_confirmation_email()
+            return redirect('contact_view')
+
         else:
-            messages.error(request, 'Meessage failed, make sure the form is valid.')
+            messages.error(request, 'Message failed, make sure the form is valid.')
     else:
         form = ContactForm()
 
     template = 'contact/contact.html'
     context = {
         'form': form,
-         'on_contact_view': True,
+        'on_contact_view': True,
     }
     return render(request, 'contact/contact.html', context)
+
+
