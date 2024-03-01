7ce924d4acb1494320583b72d88099a7ec4ca4ae
ElinaBoman <elinaholmberg@hotmail.com>
2024-03-01 12:16:06 +0000

    Trying to add mailchimp test

--- wishlist/models.py
+++ wishlist/models.py
@@ -6,8 +6,12 @@ import datetime
 class WishList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField(default=1)
+    wish_quantity = models.SmallIntegerField(default=1)
     wish_created = models.DateField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return f'{self.quantity} of {self.product}'
+        return f'{self.wish_quantity} of {self.product}'
+
+
+    def get_item_price(self):
+        return self.wish_quantity * self.product.price
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, reverse,  HttpResponse, get_object_or_404
 from .models import WishList
+from products.models import Product
+from django.contrib.auth.models import User
+
 from django.contrib import messages
 
 def wishlist_view(request):
@@ -13,21 +16,19 @@ def add_product_to_wishlist(request, item_id):
     ''''
     View to add products to wishlist
     '''
-    wish_product = WishList.objects.get(pk=item_id)
-    wishlist = request.session.get('wishlist', {})
-    
-    if item_id in wishlist:
-        wishlist[item_id] += 1
-        messages.success(request, f'Updated {wish_product.name} quantity to {wishlist[item_id]}')
-    else:
-        wishlist[item_id] = wishlist
-        messages.success(request, f'Added {wish_product.name} to basket')
-        print('wish', wish_product)
-        request.session['wishlist'] = wishlist
-        print('wishlist', wishlist)
+    wishlist = Product.objects.get(pk=item_id)
+    if request.method == 'POST':
+        WishList.save()
+        
+    quantity = 0
+    print('description', wishlist.description)
+    print('wishlist', wishlist)
 
-    template = 'products/products.html' 
-    return render(request, template)
+
+
+    return redirect('wishlist_view')
+
+
     
 
         
