edc0c652d758c54b31390c442554c3bc229d498c
ElinaBoman <elinaholmberg@hotmail.com>
2024-03-04 13:07:21 +0000

    test email

--- rootme/settings.py
+++ rootme/settings.py
@@ -221,7 +221,7 @@ STANDARD_DELIVERY_PERCENTAGE = 15
 # Email
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = 'rootme@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
