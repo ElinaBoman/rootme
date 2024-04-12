1b84f7c4e4121e0206e473a6b32d74c6ca13ee82
ElinaBoman <elinaholmberg@hotmail.com>
2024-04-12 14:48:45 +0000

    test

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -14,8 +14,8 @@ if os.path.exists("env.py"):
 def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
-    wh_secret = settings.STRIPE_WH_SECRET
-    stripe.api_key = settings.STRIPE_SECRET_KEY
+    wh_secret = os.environ.get('STRIPE_WH_SECRET')
+    stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
 
     # Get the webhook data and verify its signature
     payload = request.body
