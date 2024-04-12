097d50cd73ac2b42bebe4666df9540da347de773
ElinaBoman <elinaholmberg@hotmail.com>
2024-04-12 12:15:27 +0000

    Changing formating in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -106,8 +106,8 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Unhandled webhook received: {event["type"]} |'
-                        f' SUCCESS: Verified order already in database',
+                content=(f'Unhandled webhook received: {event["type"]} | SUCCESS: '
+                        'Verified order already in database'),
                 status=200)
         else:
             order = None
@@ -144,9 +144,9 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}| \
-                     SUCCESS: Created order in webhook',
-                    status=200)
+            content=(f'Webhook received: {event["type"]}| SUCCESS: '
+                     'Created order in webhook'),
+            status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """
