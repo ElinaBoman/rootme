a3a6fde4f5e3ebf9df8c5176db64f31bae0483b5
ElinaBoman <elinaholmberg@hotmail.com>
2024-04-12 13:13:23 +0000

    test

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -39,7 +39,6 @@ class StripeWH_Handler:
         """
         Handle a generic/unknown/unexpected webhook event
         """
-        print('is handle_event runned twice?')
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
